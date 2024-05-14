e072bf0bfb2a8eaca31729d865380dc3aaa548e4
alex <alex@pchoice.co.za>
2024-05-14 23:08:55 +0200

    chore: Add navigation links to header component

--- components/header.tsx
+++ components/header.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { motion } from "framer-motion";
+import { links } from "@/lib/data";
 
 export default function Header() {
   return (
@@ -11,6 +12,10 @@ export default function Header() {
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
+
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul></ul>
+      </nav>
     </header>
   );
 }
