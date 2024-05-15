2e7059b2e8ad0f4fb3875f4c87e5d57d91d6516f
alex <alex@pchoice.co.za>
2024-05-15 11:13:32 +0200

    chore: Update layout component with additional styles and elements

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,20 +1,28 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import profilePicture from "../public/profilePicture.jpg";
+import { motion } from "framer-motion";
 
 export default function Intro() {
   return (
     <section>
       <div className="flex items-center justify-center">
-        <div>
-          <Image
-            src={require("../public/profilePicture.jpg")}
-            alt="Alex Profile Picture"
-            width="192"
-            height="192"
-            quality={95}
-            priority={true}
-            className="rounded-full"
-          />
+        <div className="relative">
+          <motion.div>
+            <Image
+              src={profilePicture}
+              alt="Alex Profile Picture"
+              width="192"
+              height="192"
+              quality={95}
+              priority={true}
+              className="object-cover shadow-xl w-24 h-24 border-[0.35rem] border-white rounded-full"
+            />
+          </motion.div>
+
+          <span className="absolute bottom-0 right-0 text-4xl">👋</span>
         </div>
       </div>
     </section>
