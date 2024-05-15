d4cdd3a10753ee0f68a4d1d3879a855eb178e44f
alex <alex@pchoice.co.za>
2024-05-15 12:07:59 +0200

    chore: Update intro component with motion animations and improve content
    The intro component has been updated to include motion animations for a more engaging user experience. Additionally, the content has been improved to provide a clearer description of the developer's skills and interests.

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,10 +4,16 @@ import React from "react";
 import Image from "next/image";
 import profilePicture from "../public/profilePicture.jpg";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem]">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -42,7 +48,7 @@ export default function Intro() {
         </div>
       </div>
       <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl text-center"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
@@ -50,6 +56,47 @@ export default function Intro() {
         <span className="font-bold">Web-Developer developer</span>. I enjoy
         building <span className="italic">Websites & Web Apps</span>.
       </motion.h1>
+      <motion.div
+        className="flex flex-col items-center justify-center gap-2 px-4 text-lg font-medium sm:flex-row"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="flex items-center gap-2 py-3 text-white transition bg-gray-900 rounded-full outline-none group px-7 focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="transition opacity-70 group-hover:translate-x-1" />
+        </Link>
+
+        <a
+          className="flex items-center gap-2 py-3 transition bg-white rounded-full outline-none cursor-pointer group px-7 focus:scale-110 hover:scale-110 active:scale-105 borderBlack "
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="transition opacity-60 group-hover:translate-y-1" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack"
+          href="https://www.linkedin.com/in/alexvdberg010109/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack "
+          href="https://github.com/alex-dev742"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
