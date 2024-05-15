66fe8bc98f20e6101be1789f51bbaa7317bfbf01
alex <alex@pchoice.co.za>
2024-05-15 13:13:42 +0200

    chore: Add Projects component to Home page
    The Home page has been updated to include the Projects component. This adds a new section to showcase the developer's projects.

---
+++ components/projects.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeading>My projects</SectionHeading>
+
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+type ProjectProps = (typeof projectsData)[0];
+
+function Project({ title, description, tags, imageUrl }: ProjectProps) {
+  return <div>{title}</div>;
+}
