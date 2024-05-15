c292d7fa7cb3a654cb4d3b7903a03bbd9b4b0fa1
alex <alex@pchoice.co.za>
2024-05-15 11:54:14 +0200

    chore: Update intro component with motion animations and improve content
    The intro component has been updated to include motion animations for a more engaging user experience. Additionally, the content has been improved to provide a clearer description of the developer's skills and interests.

--- components/intro.tsx
+++ components/intro.tsx
@@ -41,6 +41,15 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Alex.</span> I'm a{" "}
+        <span className="font-bold">Web-Developer developer</span>. I enjoy
+        building <span className="italic">Websites & Web Apps</span>.
+      </motion.h1>
     </section>
   );
 }
