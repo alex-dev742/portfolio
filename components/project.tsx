773e505f006cd0a12889660fbf9de3417e4c7915
Alex <alexvdberg010109@gmail.com>
2024-05-16 11:09:20 +0200

    refactor: Update Project component with motion animations and lazy loading for images

--- components/project.tsx
+++ components/project.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { projectsData } from "@/lib/data";
 import { useScroll } from "framer-motion";
 import { useRef } from "react";
+import { motion } from "framer-motion";
 
 type ProjectProps = (typeof projectsData)[0];
 
@@ -13,31 +14,40 @@ export default function Project({
   tags,
   imageUrl,
 }: ProjectProps) {
-  const ref = useRef(null);
-  useScroll({ target: "", offset: ["0 1", "1.33 1"] });
+  const ref = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
 
   return (
-    <section className="group bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8 even:pl-8 hover:bg-gray-200 transition mb-3 sm:mb-8 last:mb-0">
-      <div className="px-5 pt-4 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full mb-3 sm:mb-8 last:mb-0 group-even:ml-[18rem]">
-        <h3 className="text-2xl font-semibold">{title}</h3>
-        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
-        <ul className="flex flex-wrap gap-2 mt-4 sm:mt-auto">
-          {tags.map((tag, index) => (
-            <li
-              className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
-              key={index}
-            >
-              {tag}
-            </li>
-          ))}
-        </ul>
-        <Image
-          className="absolute top-8 -right-40 w-[28.25rem] transition rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
-          src={imageUrl}
-          alt="Projects I worked on"
-          quality={95}
-        />
-      </div>
-    </section>
+    <motion.div
+      ref={ref}
+      style={{ scale: scrollYProgress, opacity: scrollYProgress }}
+      className="mb-3 sm:mb-8 last:mb-0"
+    >
+      <section className="group bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8 even:pl-8 hover:bg-gray-200 transition">
+        <div className="px-5 pt-4 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full mb-3 sm:mb-8 last:mb-0 group-even:ml-[18rem]">
+          <h3 className="text-2xl font-semibold">{title}</h3>
+          <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+          <ul className="flex flex-wrap gap-2 mt-4 sm:mt-auto">
+            {tags.map((tag, index) => (
+              <li
+                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+                key={index}
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
+          <Image
+            className="absolute top-8 -right-40 w-[28.25rem] transition rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-hover:scale-[1.04] group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
+            src={imageUrl}
+            alt="Projects I worked on"
+            quality={95}
+          />
+        </div>
+      </section>
+    </motion.div>
   );
 }
