9c77c7aff88949feb875a2e27db92a6d8c671ed4
Alex <alexvdberg010109@gmail.com>
2024-05-16 11:16:46 +0200

    refactor: Update Project component with motion animations, lazy loading for images, and improve scroll effects

--- components/project.tsx
+++ components/project.tsx
@@ -26,9 +26,9 @@ export default function Project({
     <motion.div
       ref={ref}
       style={{ scale: scaleProgress, opacity: opacityProgress }}
-      className="mb-3 sm:mb-8 last:mb-0"
+      className="mb-3 group sm:mb-8 last:mb-0"
     >
-      <section className="group bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8 even:pl-8 hover:bg-gray-200 transition">
+      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] sm:group-even:pl-8 group-even:even:pl-8 hover:bg-gray-200 transition">
         <div className="px-5 pt-4 pb-7 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full mb-3 sm:mb-8 last:mb-0 group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
