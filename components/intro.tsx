9f447da448166559b65a397e5c518deb5ac4482b
alex-dev742 <158592162+alex-dev742@users.noreply.github.com>
2024-05-20 15:39:18 +0200

    refactor: Update About, Intro, and Projects components with intersection observer for section visibility

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import { useEffect } from "react";
 import Image from "next/image";
 import profilePicture from "../public/profilePicture.jpg";
 import { motion } from "framer-motion";
@@ -8,10 +8,21 @@ import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
+  const { ref, inView } = useInView({ threshold: 0.5 });
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("Home");
+    }
+  }, [inView, setActiveSection]);
   return (
     <section
+      ref={ref}
       className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
       id="home"
     >
