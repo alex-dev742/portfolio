2794b18c4c0ae06759f112d98201ba0c7d571700
alex-dev742 <158592162+alex-dev742@users.noreply.github.com>
2024-05-21 11:49:32 +0200

    refactor: Update global CSS to set line color and add Experience component to Home page

--- components/skills.tsx
+++ components/skills.tsx
@@ -24,10 +24,11 @@ export default function Skills() {
   const { ref } = useSectionInView("Skills");
   return (
     <section
+      id="skills"
       ref={ref}
       className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
     >
-      <SectionHeading>My Skills</SectionHeading>
+      <SectionHeading>My skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skills, index) => (
           <motion.li
