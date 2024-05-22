a0542607d855b03ee035c3a99513da0cdeb562c1
alex-dev742 <158592162+alex-dev742@users.noreply.github.com>
2024-05-22 16:57:46 +0200

    Updated Data

--- lib/data.ts
+++ lib/data.ts
@@ -64,8 +64,7 @@ export const experiencesData = [
 export const projectsData = [
   {
     title: "CorpComment",
-    description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    description: "I worked as a full-stack developer on this project",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
     imageUrl: corpcommentImg,
   },
